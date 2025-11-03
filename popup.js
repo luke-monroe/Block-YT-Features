496c979548a0822a60eec2509e39ed74dee789a6
Luke Monroe <monroel@andrews.edu>
2025-11-03 17:20:05 -0500

    Change storing and load settings from storage

--- popup.js
+++ popup.js
@@ -7,8 +7,13 @@ const shortsBox = document.getElementById("shorts")
 const thumbnailsBox = document.getElementById("thumbnails")
 // Store checkbox values
 settingsForm.addEventListener("change", (event) => {
-  settings.shorts = shortsBox.checked;
-  settings.thumbnails = thumbnailsBox.checked;
-  chrome.storage.local.set({settings});
-  console.log("Settings: ", settings)
+  shorts = shortsBox.checked;
+  thumbnails = thumbnailsBox.checked;
+  chrome.storage.local.set({"shorts":shorts, "thumbnails": thumbnails});
 }); 
+// Load settings from storage
+chrome.storage.local.get(["shorts", "thumbnails"]).then((data) => {
+  // console.log("shorts " + result.shorts);
+  shortsBox.checked = data.shorts
+  thumbnailsBox.checked = data.thumbnails
+});
