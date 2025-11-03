7caf5533d7d4441dca59b8845334bed467ffd0da
Luke Monroe <monroel@andrews.edu>
2025-11-03 17:02:00 -0500

    Save checkbox settings to storage

--- popup.js
+++ popup.js
@@ -1,4 +1,14 @@
-console.log("Subscribe to Virej!");
-console.log(
-  "https://www.youtube.com/channel/UCRzS48bskynAxoI38BFypfQ?sub_confirmation=1"
-);
+// JS Functionality here
+
+// Storage
+const settings = {};
+const settingsForm = document.getElementById("settingsForm");
+const shortsBox = document.getElementById("shorts")
+const thumbnailsBox = document.getElementById("thumbnails")
+// Store checkbox values
+settingsForm.addEventListener("change", (event) => {
+  settings.shorts = shortsBox.checked;
+  settings.thumbnails = thumbnailsBox.checked;
+  chrome.storage.local.set({settings});
+  console.log("Settings: ", settings)
+}); 
